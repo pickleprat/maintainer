baf12f3395acf6e78ba195ee79517bfd6c3ce650
pickleprat <prat16here@gmail.com>
2025-05-12 11:25:25 +0530

    Added spring web to dependency

--- src/main/java/com/habbits/maintainer/MaintainerApplication.java
+++ src/main/java/com/habbits/maintainer/MaintainerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaintainerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MaintainerApplication.class, args);
 	}
-
 }
